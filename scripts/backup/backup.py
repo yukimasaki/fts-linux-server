2232cd1a150f26e8b4aa74190a1bf07a10a35001
yukimasaki <woodenhoe@gmail.com>
2023-05-02 14:56:26 +0900

    issue: 複数仮想マシンのバックアップ処理を実装、Windows仮想マシンのバックアップに失敗する

--- scripts/backup/backup.py
+++ scripts/backup/backup.py
@@ -15,7 +15,8 @@ def main():
 
     # バックアップ対象の仮想マシン名を配列で指定
     vms = [
-        'test-ubuntu'
+        {'name': 'test-ubuntu', 'disk': 'vda'},
+        {'name': 'test-win', 'disk': 'sda'}
     ]
     
     # バックアップ先を指定
@@ -34,58 +35,51 @@ def main():
 
     for vm in vms:
         try:
-            backup_image_path = f'{backup_destination_path}/{vm}_{today}.qcow2'
+            backup_image_path = f'{backup_destination_path}/{vm["name"]}_{today}.qcow2'
             # 当日のバックアップイメージファイルが既に存在する場合は処理を中断
             if os.path.isfile(backup_image_path):
-                result = {'vm': vm, 'status': '中断', 'subject': 'バックアップは既に存在します', 'body': ''}
-                print(result)
+                result = {'vm': vm["name"], 'status': '中断', 'subject': 'バックアップは既に存在します', 'body': ''}
             # 存在しない場合は処理を続行
-            else:
+            else:               
                 # xmlファイルを作成
-                xml_file_name = f'{vm}_{today}.xml'
-                xml_file_path = create_backup_xml(backup_destination_path, xml_file_name, backup_image_path)
+                xml_file_name = f'{vm["name"]}_{today}.xml'
+                xml_file_path = create_backup_xml(vm, backup_destination_path, xml_file_name, backup_image_path)
 
                 # バックアップを開始
-                begin_backup = f'sudo virsh backup-begin {vm} --backupxml {xml_file_path}'
+                begin_backup = f'sudo virsh backup-begin {vm["name"]} --backupxml {xml_file_path}'
                 subprocess.run(begin_backup.split())
 
                 # 進捗の確認を開始
-                # 「Job type: Unbounded」である間は処理を継続し、「Job type: None」
+                # 「Job type: Unbounded」である間は処理を継続し、それ以外の結果の場合は次の処理へ移行する
                 job_running = True
                 while job_running:
                     is_working = 'sudo virsh domjobinfo test-ubuntu'
                     result = subprocess.run(is_working.split(), capture_output=True, text=True)
                     job_running = 'Job type:         Unbounded' in result.stdout                    
-
-                    if not job_running:
-                        break
-                        
-                    print('Job type:         Unbounded')
+                    if not job_running: break
                     time.sleep(10)
 
-                # コード2を実行する
+                # 「Job type: Completed」の場合は処理を正常終了し、それ以外の結果の場合は例外をスローする
                 is_completed = 'sudo virsh domjobinfo test-ubuntu --completed'
                 result = subprocess.run(is_completed.split(), capture_output=True, text=True)
 
                 if 'Job type:         Completed' in result.stdout:
-                    print('完了')
+                    result = {'vm': vm["name"], 'status': '成功', 'subject': 'バックアップが正常に終了しました', 'body': 'body'}
                 else:
-                    print('エラー')
+                    raise Exception
 
-                # 完了したら処理を終了
-                result = {'vm': vm, 'status': '成功', 'subject': 'バックアップが正常に終了しました', 'body': 'body'}
-                print(result)
         except Exception as ex:
-            result = {'vm': vm, 'status': '失敗', 'subject': f'予期しないエラーによりバックアップが失敗しました', 'body': ex}            
-            print(result)
+            result = {'vm': vm["name"], 'status': '失敗', 'subject': f'予期しないエラーによりバックアップが失敗しました', 'body': ex}            
+            
         finally:
             subject = f'[{customer}][{result["vm"]}][{result["status"]}]{result["subject"]}'
             message = f'ステータス: {result["status"]}\n結果: {result["subject"]}\n{result["body"]}'
 
-            mime = send_email.create_mime_text(from_email, to_email, message, subject)
-            # send_email.send_email(mime)
+            # mime = send_email.create_mime_text(from_email, to_email, message, subject)
+            # # send_email.send_email(mime)
+            print(result)
 
-def create_backup_xml(backup_destination_path, xml_file_name, backup_image_path):
+def create_backup_xml(vm, backup_destination_path, xml_file_name, backup_image_path):
     # 親要素「domainbackup」を作成
     domainbackup = ET.Element("domainbackup")
     
@@ -95,7 +89,7 @@ def create_backup_xml(backup_destination_path, xml_file_name, backup_image_path)
     
     # 孫要素「disk」を作成し属性値を設定する
     disk = ET.Element("disk")
-    disk.set("name", "vda")
+    disk.set("name", vm["disk"])
     disk.set("backup", "yes")
     disk.set("type", "file")
     
