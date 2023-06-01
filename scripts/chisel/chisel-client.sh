70952236ae12197055ffc0ed8b1dd08351d3c60b
yukimasaki <woodenhoe@gmail.com>
2023-06-01 09:38:09 +0900

    update: Unitファイル > ExecStartにコマンドを直接記述した

--- scripts/chisel/chisel-client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /opt/fts-lsmt/scripts/chisel/.env
-chisel client "$CHISEL_SERVER" "$REMOTE"
--- scripts/chisel/chisel-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /opt/fts-lsmt/scripts/chisel/.env
-chisel server --reverse --port 443 --tls-domain "$TLS_DOMAIN"
